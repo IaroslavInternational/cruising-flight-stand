593c566065f6017f11852ff867b7c4e86bc5d35b
Iaroslav <IaroslavInternational@yandex.ru>
2022-12-06 16:41:57 +0300

    Добавьте файлы проекта.

---
+++ CruisingFlightStand/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace CruisingFlightStand
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.tenzo1_data = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.tenzo2_data = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel8 = new MaterialSkin.Controls.MaterialLabel();
+            this.tenzo3_data = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel6 = new MaterialSkin.Controls.MaterialLabel();
+            this.tenzo4_data = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel10 = new MaterialSkin.Controls.MaterialLabel();
+            this.tenzo5_data = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(12, 121);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(117, 19);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "Тензодатчик 1:";
+            // 
+            // tenzo1_data
+            // 
+            this.tenzo1_data.AutoSize = true;
+            this.tenzo1_data.Depth = 0;
+            this.tenzo1_data.Font = new System.Drawing.Font("Roboto", 11F);
+            this.tenzo1_data.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tenzo1_data.Location = new System.Drawing.Point(135, 121);
+            this.tenzo1_data.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tenzo1_data.Name = "tenzo1_data";
+            this.tenzo1_data.Size = new System.Drawing.Size(63, 19);
+            this.tenzo1_data.TabIndex = 1;
+            this.tenzo1_data.Text = "5500 гр.";
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.AutoSize = true;
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(12, 145);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(117, 19);
+            this.materialLabel4.TabIndex = 2;
+            this.materialLabel4.Text = "Тензодатчик 2:";
+            // 
+            // tenzo2_data
+            // 
+            this.tenzo2_data.AutoSize = true;
+            this.tenzo2_data.Depth = 0;
+            this.tenzo2_data.Font = new System.Drawing.Font("Roboto", 11F);
+            this.tenzo2_data.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tenzo2_data.Location = new System.Drawing.Point(135, 145);
+            this.tenzo2_data.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tenzo2_data.Name = "tenzo2_data";
+            this.tenzo2_data.Size = new System.Drawing.Size(63, 19);
+            this.tenzo2_data.TabIndex = 3;
+            this.tenzo2_data.Text = "5500 гр.";
+            // 
+            // materialLabel8
+            // 
+            this.materialLabel8.AutoSize = true;
+            this.materialLabel8.Depth = 0;
+            this.materialLabel8.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel8.Location = new System.Drawing.Point(12, 169);
+            this.materialLabel8.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel8.Name = "materialLabel8";
+            this.materialLabel8.Size = new System.Drawing.Size(117, 19);
+            this.materialLabel8.TabIndex = 4;
+            this.materialLabel8.Text = "Тензодатчик 3:";
+            // 
+            // tenzo3_data
+            // 
+            this.tenzo3_data.AutoSize = true;
+            this.tenzo3_data.Depth = 0;
+            this.tenzo3_data.Font = new System.Drawing.Font("Roboto", 11F);
+            this.tenzo3_data.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tenzo3_data.Location = new System.Drawing.Point(135, 169);
+            this.tenzo3_data.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tenzo3_data.Name = "tenzo3_data";
+            this.tenzo3_data.Size = new System.Drawing.Size(63, 19);
+            this.tenzo3_data.TabIndex = 5;
+            this.tenzo3_data.Text = "5500 гр.";
+            // 
+            // materialLabel6
+            // 
+            this.materialLabel6.AutoSize = true;
+            this.materialLabel6.Depth = 0;
+            this.materialLabel6.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel6.Location = new System.Drawing.Point(12, 193);
+            this.materialLabel6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel6.Name = "materialLabel6";
+            this.materialLabel6.Size = new System.Drawing.Size(117, 19);
+            this.materialLabel6.TabIndex = 6;
+            this.materialLabel6.Text = "Тензодатчик 4:";
+            // 
+            // tenzo4_data
+            // 
+            this.tenzo4_data.AutoSize = true;
+            this.tenzo4_data.Depth = 0;
+            this.tenzo4_data.Font = new System.Drawing.Font("Roboto", 11F);
+            this.tenzo4_data.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tenzo4_data.Location = new System.Drawing.Point(135, 193);
+            this.tenzo4_data.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tenzo4_data.Name = "tenzo4_data";
+            this.tenzo4_data.Size = new System.Drawing.Size(63, 19);
+            this.tenzo4_data.TabIndex = 7;
+            this.tenzo4_data.Text = "5500 гр.";
+            // 
+            // materialLabel10
+            // 
+            this.materialLabel10.AutoSize = true;
+            this.materialLabel10.Depth = 0;
+            this.materialLabel10.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel10.Location = new System.Drawing.Point(12, 217);
+            this.materialLabel10.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel10.Name = "materialLabel10";
+            this.materialLabel10.Size = new System.Drawing.Size(117, 19);
+            this.materialLabel10.TabIndex = 8;
+            this.materialLabel10.Text = "Тензодатчик 5:";
+            // 
+            // tenzo5_data
+            // 
+            this.tenzo5_data.AutoSize = true;
+            this.tenzo5_data.Depth = 0;
+            this.tenzo5_data.Font = new System.Drawing.Font("Roboto", 11F);
+            this.tenzo5_data.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.tenzo5_data.Location = new System.Drawing.Point(135, 217);
+            this.tenzo5_data.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tenzo5_data.Name = "tenzo5_data";
+            this.tenzo5_data.Size = new System.Drawing.Size(63, 19);
+            this.tenzo5_data.TabIndex = 9;
+            this.tenzo5_data.Text = "5500 гр.";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(144, 256);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(64, 19);
+            this.materialLabel2.TabIndex = 11;
+            this.materialLabel2.Text = "5500 Па";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(12, 256);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(126, 19);
+            this.materialLabel3.TabIndex = 10;
+            this.materialLabel3.Text = "Сопротивление:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 531);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.tenzo5_data);
+            this.Controls.Add(this.materialLabel10);
+            this.Controls.Add(this.tenzo4_data);
+            this.Controls.Add(this.materialLabel6);
+            this.Controls.Add(this.tenzo3_data);
+            this.Controls.Add(this.materialLabel8);
+            this.Controls.Add(this.tenzo2_data);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.tenzo1_data);
+            this.Controls.Add(this.materialLabel1);
+            this.Name = "MainForm";
+            this.Text = "Cruising Flight Stand";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel tenzo1_data;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialLabel tenzo2_data;
+        private MaterialSkin.Controls.MaterialLabel materialLabel8;
+        private MaterialSkin.Controls.MaterialLabel tenzo3_data;
+        private MaterialSkin.Controls.MaterialLabel materialLabel6;
+        private MaterialSkin.Controls.MaterialLabel tenzo4_data;
+        private MaterialSkin.Controls.MaterialLabel materialLabel10;
+        private MaterialSkin.Controls.MaterialLabel tenzo5_data;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+    }
+}
+
---
+++ CruisingFlightStand/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+
+namespace CruisingFlightStand
+{
+    public partial class MainForm : MaterialForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
